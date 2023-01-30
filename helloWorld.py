640f95c0865fd9e8709bbb5a44a619615fedf767
Xuan Thinh <thinhhotboyct10012000@gmail.com>
2023-01-30 11:46:15 +0700

    Modified greeting()

--- helloWorld.py
+++ helloWorld.py
@@ -1,10 +1,12 @@
 def greeting(name):
     print(f"Hello {name}")
+    print("Have a good day")
 
 def main():
     print("Enter your name: ", end='')
     name = input()
     greeting(name)
+
 
 if __name__ == "__main__":
     main()
